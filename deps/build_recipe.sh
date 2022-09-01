72c8d6deae84b664a7a2197db02b312127dfb239
jaysinco <jaysinco@163.com>
2022-09-01 16:37:26 +0800

    create package at local dir

--- deps/build_recipe.sh
+++ deps/build_recipe.sh
@@ -27,34 +27,79 @@ CONAN_REF="jaysinco/stable"
 CONAN_PROFILE="$PROJECT_ROOT/config/$PLATFORM/$ARCH/conan.profile"
 CONAN_CONF="tools.cmake.cmaketoolchain:generator=$GENERATOR"
 
+function conan_install() {
+    conan install \
+        --profile=$CONAN_PROFILE \
+        --profile:build=$CONAN_PROFILE \
+        --conf=$CONAN_CONF \
+        . $CONAN_REF
+}
+
+function conan_source() {
+    conan source .
+}
+
+function conan_build() {
+    conan build .
+}
+
+function conan_export_pkg() {
+    conan export-pkg \
+        --force \
+        . $CONAN_REF
+}
+
+function conan_create() {
+    conan create \
+        --profile=$CONAN_PROFILE \
+        --profile:build=$CONAN_PROFILE \
+        --conf=$CONAN_CONF \
+        . $CONAN_REF
+}
+
+function clean_build() {
+    git clean -fdx .
+}
+
 while [[ $# -gt 0 ]]; do
     case $1 in
         -h|--help)
             echo
-            echo "Usage: build.sh [options] [targets]"
+            echo "Usage: build.sh [options]"
             echo
             echo "Options:"
-            echo "  -i, --install    conan install"
-            echo "  -s, --source     conan source"
-            echo "  -b, --build      conan build"
-            echo "  -h, --help       print command line options"
+            echo "  -i, --install       conan install"
+            echo "  -s, --source        conan source"
+            echo "  -b, --build         conan build"
+            echo "  -e, --export-pkg    conan export-pkg"
+            echo "      --create        conan create"
+            echo "  -c, --clean         clean build output"
+            echo "  -h, --help          print command line options"
             echo
-            exit 0
-            ;;
-        -s|--source)
-            conan source .
             exit 0
             ;;
         -i|--install)
-            conan install \
-                --profile=$CONAN_PROFILE \
-                --profile:build=$CONAN_PROFILE \
-                --conf=$CONAN_CONF \
-                . $CONAN_REF
+            conan_install
+            exit 0
+            ;;
+        -s|--source)
+            conan_source
             exit 0
             ;;
         -b|--build)
-            conan build .
+            conan_build
+            exit 0
+            ;;
+        -e|--export-pkg)
+            conan_export_pkg
+            exit 0
+            ;;
+        --create)
+            conan_create
+            exit 0
+            ;;
+        -c|--clean)
+            clean_build
             exit 0
             ;;
         -*|--*)
@@ -64,8 +109,8 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-conan create \
-    --profile=$CONAN_PROFILE \
-    --profile:build=$CONAN_PROFILE \
-    --conf=$CONAN_CONF \
-    . $CONAN_REF
+clean_build \
+&& conan_install \
+&& conan_source \
+&& conan_build \
+&& conan_export_pkg
