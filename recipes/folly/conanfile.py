891c08fb94990a1e3d8a6f29bb90bb283c83e307
jaysinco <jaysinco@163.com>
2022-09-16 23:21:39 +0800

    add folly

--- recipes/folly/conanfile.py
+++ recipes/folly/conanfile.py
@@ -4,6 +4,7 @@ from conans import ConanFile, tools
 from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps
 from conan.tools.microsoft import msvc_runtime_flag, is_msvc
 from conan.tools.files import collect_libs, copy, rmdir
+from conans.errors import ConanInvalidConfiguration
 
 
 class FollyConan(MyConanFile):
@@ -31,6 +32,11 @@ class FollyConan(MyConanFile):
         if self.options.shared:
             del self.options.fPIC
 
+    def validate(self):
+        if self.settings.os in ["Macos", "Windows"] and self.options.shared:
+            raise ConanInvalidConfiguration(
+                "Folly could not be built on {} as shared library".format(self.settings.os))
+
     def requirements(self):
         self.requires(self._ref_pkg("boost/1.79.0"))
         self.requires(self._ref_pkg("bzip2/1.0.8"))
@@ -44,10 +50,20 @@ class FollyConan(MyConanFile):
         self.requires(self._ref_pkg("zlib/1.2.12"))
         self.requires(self._ref_pkg("zstd/1.5.2"))
         self.requires(self._ref_pkg("fmt/8.1.1"))
+        # self.requires("libsodium/1.0.18")
+        # self.requires("xz_utils/5.2.5")
+        # if not is_msvc(self):
+        #     self.requires("libdwarf/20191104")
+        # if self.settings.os == "Linux":
+        #     self.requires("libiberty/9.1.0")
+        #     self.requires("libunwind/1.5.0")
 
     def source(self):
         srcFile = self._src_abspath(f"{self.name}-{self.version}.tar.gz")
         tools.unzip(srcFile, destination=self.source_folder, strip_root=False)
+        self._patch_sources(self._dirname(__file__), [
+            "0001-fix-windows-minmax.patch",
+        ])
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -90,6 +106,30 @@ class FollyConan(MyConanFile):
         self.cpp_info.set_property("cmake_file_name", "folly")
         self.cpp_info.set_property("cmake_target_name", "Folly::folly")
         self.cpp_info.set_property("pkg_config_name", "libfolly")
+        self.cpp_info.libs = collect_libs(self, folder="lib")
+        self.cpp_info.requires = [
+            "bzip2::bzip2",
+            "double-conversion::double-conversion",
+            "gflags::gflags",
+            "glog::glog",
+            "libevent::libevent",
+            "lz4::lz4",
+            "openssl::openssl",
+            "snappy::snappy",
+            "zlib::zlib",
+            "zstd::zstd",
+            "fmt::fmt",
+            # "libsodium::libsodium",
+            # "xz_utils::xz_utils"
+        ]
+        # if not is_msvc(self):
+        #     self.cpp_info.requires.append("libdwarf::libdwarf")
+        if self.settings.os == "Windows":
+            self.cpp_info.system_libs.extend(["ws2_32", "iphlpapi", "crypt32"])
+        elif self.settings.os == "Linux":
+            # self.cpp_info.requires.extend(["libiberty::libiberty", "libunwind::libunwind"])
+            # self.cpp_info.defines.extend(["FOLLY_HAVE_ELF", "FOLLY_HAVE_DWARF"])
+            self.cpp_info.system_libs.extend(["pthread", "dl", "rt"])
 
     def _cmake_path(self):
         prefix_path = []
