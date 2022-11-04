d8cc8839e0c55ce5eb6c196c9cc9fd96956f3de3
jaysinco <jaysinco@163.com>
2022-11-04 13:59:46 +0800

    add thread pool

--- build.sh
+++ build.sh
@@ -137,6 +137,8 @@ if [ $do_build_all -eq 1 ]; then
     && package 1 cpr \
     && package 1 sqlite3 \
     && package 1 mongoose \
+    && package 0 concurrentqueue \
+    && package 0 threadpool \
     && echo done!
     exit 0
 fi
