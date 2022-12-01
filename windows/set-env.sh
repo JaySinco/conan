c91a9da5a50d6ad8717a306d705a55b61f9b8dea
jaysinco <jaysinco@163.com>
2022-12-01 21:48:32 +0800

    add nvim version

--- windows/set-env.sh
+++ windows/set-env.sh
@@ -5,6 +5,8 @@ set -e
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 program_files_dir=$USERPROFILE/ProgramFiles
 source_repo=$USERPROFILE/OneDrive/src
+nvim_version="0.7.2"
+nvim_data_dir=$LOCALAPPDATA/nvim-data
 
 # setup msys
 # -----------------
@@ -29,14 +31,13 @@ fi
 if [ ! -d $program_files_dir/nvim-win64 ]; then
     echo "copy nvim"
     mkdir -p $program_files_dir
-    unzip -q $source_repo/nvim-0.7.2-windows-x86_64.zip -d $program_files_dir
+    unzip -q $source_repo/nvim-v$nvim_version-windows-x86_64.zip -d $program_files_dir
 fi
 
-nvim_data_dir=$LOCALAPPDATA/nvim-data
 if [ ! -d $nvim_data_dir/site ]; then
     echo "copy nvim data"
     mkdir -p $nvim_data_dir
-    unzip -q $source_repo/nvim-data-site-v2022.09.24-$os.zip -d $nvim_data_dir
+    unzip -q $source_repo/nvim-data-site-v$nvim_version-windows.zip -d $nvim_data_dir
 fi
 
 if [ ! -d $program_files_dir/lua-language-server ]; then
